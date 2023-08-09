b4d1dc02509c8e9ecbd213e7fa9e7790ddb3387d
erazor <52050311+erazoor@users.noreply.github.com>
2023-08-09 03:14:48 +0200

    feat(filters): all filters implemented

--- src/components/FilterTab.vue
+++ src/components/FilterTab.vue
@@ -1,7 +1,169 @@
+<script setup>
+
+</script>
+
 <template>
-    FilterTab
+    <div>
+        <h2>{{ countSelectedFilters > 1 ? `Sexes (${countSelectedFilters})` : `Sexe (${countSelectedFilters})` }}</h2>
+        <div>
+            <input type="checkbox" name="men" @change="checkFilters($event)" id="men" checked>
+            <span>Hommes</span>
+        </div>
+        <div>
+            <input type="checkbox" name="women" @change="checkFilters($event)" id="women" checked>
+            <span>Femmes</span>
+        </div>
+        <div>
+            <input type="checkbox" name="unisex" @change="checkFilters($event)" id="unisex" checked>
+            <span>Mixte</span>
+        </div>
+    </div>
+    <div>
+        <h2>Rechercher par prix ({{ countSelectedRanges }})</h2>
+        <div>
+            <input type="checkbox" name="fifty" @change="checkRanges($event)" id="fifty" checked>
+            <span>Moins €50</span>
+        </div>
+        <div>
+            <input type="checkbox" name="fiftytohundred" @change="checkRanges($event)" id="fiftytohundred" checked>
+            <span>€50 - €100</span>
+        </div>
+        <div>
+            <input type="checkbox" name="hundredtofifty" @change="checkRanges($event)" id="hundredtofifty" checked>
+            <span>€100 - €150</span>
+        </div>
+        <div>
+            <input type="checkbox" name="hundredfifty" @change="checkRanges($event)" id="hundredfifty" checked>
+            <span>Plus de €150</span>
+        </div>
+    </div>
+    <div>
+        <h2>{{ countSelectedColors > 1 ? `Couleurs (${countSelectedColors})` : `Couleur (${countSelectedColors})` }}</h2>
+        <div>
+            <input type="checkbox" name="pourpre" @change="checkColors($event)" id="pourpre">
+            <span>Pourpre</span>
+        </div>
+        <div>
+            <input type="checkbox" name="noir" @change="checkColors($event)" id="noir">
+            <span>Noir</span>
+        </div>
+        <div>
+            <input type="checkbox" name="rouge" @change="checkColors($event)" id="rouge">
+            <span>Rouge</span>
+        </div>
+        <div>
+            <input type="checkbox" name="orange" @change="checkColors($event)" id="orange">
+            <span>Orange</span>
+        </div>
+        <div>
+            <input type="checkbox" name="bleu" @change="checkColors($event)" id="bleu">
+            <span>Bleu</span>
+        </div>
+        <div>
+            <input type="checkbox" name="blanc" @change="checkColors($event)" id="blanc">
+            <span>Blanc</span>
+        </div>
+        <div>
+            <input type="checkbox" name="marron" @change="checkColors($event)" id="marron">
+            <span>Marron</span>
+        </div>
+        <div>
+            <input type="checkbox" name="vert" @change="checkColors($event)" id="vert">
+            <span>Vert</span>
+        </div>
+        <div>
+            <input type="checkbox" name="jaune" @change="checkColors($event)" id="jaune">
+            <span>Jaune</span>
+        </div>
+        <div>
+            <input type="checkbox" name="gris" @change="checkColors($event)" id="gris">
+            <span>Gris</span>
+        </div>
+        <div>
+            <input type="checkbox" name="multicolore" @change="checkColors($event)" id="multicolore">
+            <span>Multicolore</span>
+        </div>
+    </div>
+    <div>
+        <h2>{{ countSelectedSports > 1 ? `Sports (${countSelectedSports})` : `Sport (${countSelectedSports})` }}</h2>
+        <div>
+            <input type="checkbox" name="basket" @change="checkSports($event)" id="basket" checked>
+            <span>Basket</span>
+        </div>
+        <div>
+            <input type="checkbox" name="football" @change="checkSports($event)" id="football" checked>
+            <span>Football</span>
+        </div>
+        <div>
+            <input type="checkbox" name="running" @change="checkSports($event)" id="running" checked>
+            <span>Running</span>
+        </div>
+    </div>
 </template>
-  
+
+<script>
+    export default {
+        name: 'FilterTab',
+        emits: ['check-filter', 'check-range', 'check-color', 'check-sport'],
+        data() {
+            return {
+                checkedFilterValues: ["men", "women", "unisex"],
+                checkedRangeValues: ['fifty', 'fiftytohundred', 'hundredtofifty', 'hundredfifty'],
+                checkedColorValues: [],
+                checkedSportValues: ['basket', 'football', 'running'],
+            }
+        },
+        methods: {
+            checkFilters(event) {
+                if(event.target.checked)
+                    this.checkedFilterValues.push(event.target.id);
+                else
+                    this.checkedFilterValues.splice(this.checkedFilterValues.indexOf(event.target.id), 1);
+
+                this.$emit('check-filter', this.checkedFilterValues);
+            },
+            checkRanges(event) {
+                if(event.target.checked)
+                    this.checkedRangeValues.push(event.target.id);
+                else
+                    this.checkedRangeValues.splice(this.checkedRangeValues.indexOf(event.target.id), 1);
+
+                this.$emit('check-range', this.checkedRangeValues);
+            },
+            checkColors(event) {
+                if(event.target.checked)
+                    this.checkedColorValues.push(event.target.id);
+                else
+                    this.checkedColorValues.splice(this.checkedColorValues.indexOf(event.target.id), 1);
+
+                this.$emit('check-color', this.checkedColorValues);
+            },
+            checkSports(event) {
+                if(event.target.checked)
+                    this.checkedSportValues.push(event.target.id);
+                else
+                    this.checkedSportValues.splice(this.checkedSportValues.indexOf(event.target.id), 1);
+
+                this.$emit('check-sport', this.checkedSportValues);
+            },
+        },
+        computed: {
+            countSelectedFilters() {
+                return this.checkedFilterValues.length;
+            },
+            countSelectedRanges() {
+                return this.checkedRangeValues.length;
+            },
+            countSelectedColors() {
+                return this.checkedColorValues.length;
+            },
+            countSelectedSports() {
+                return this.checkedSportValues.length;
+            }
+        },
+    }
+</script>
+
 <style scoped>
-  
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,104 @@
 <script setup>
-import FilterTab from '../components/FilterTab.vue';
-import ProductList from '../components/ProductList.vue';
-import HeaderBand from '../components/HeaderBand.vue';
+  import FilterTab from '../components/FilterTab.vue';
+  import ProductList from '../components/ProductList.vue';
+  import HeaderBand from '../components/HeaderBand.vue';
 </script>
 
 <template>
-  <header>
-    <HeaderBand />
+  <header class="header">
+    <header-band />
+    {{ getCurrentProductDisplayed() }}
   </header>
-  <main>
-    <aside>
-      <FilterTab />
+  <main class="main">
+    <aside class="filter-tab">
+      <filter-tab @check-color="updateDataByColor" @check-range="updateDataByRange"  @check-filter="updateDataByFilter" @check-sport="updateDataBySport"></filter-tab>
     </aside>
-    <section id="product-list">
-      <ProductList />
+    <section class="product-list" v-if="displayProductsList">
+      <product-list v-for="product in displayProductsList" :key="product.article" :productData="product"></product-list>
     </section>
   </main>
 </template>
 
+<script>
+  export default {
+    name: 'HomeView',
+    data() {
+      return {
+        filteredProducts: ['men', 'women', 'unisex'],
+        rangedProducts: ['fifty', 'fiftytohundred', 'hundredtofifty', 'hundredfifty'],
+        coloredProducts: [],
+        sportProducts: ['basket', 'football', 'running'],
+      }
+    },
+    components: {
+      FilterTab,
+      ProductList,
+      HeaderBand,
+    },
+    computed: {
+      displayProductsList() {
+        return this.$store.getters['products/displayProducts'] ? this.$store.getters['products/displayProducts'].filter(product => {
+          const productGender = product.sexe.toLowerCase();
+          const productPrice = this.getNumberFromPrice(product.prix);
+          const productSport = product.sport.toLowerCase();
+          const colorArray = product.couleur.toLowerCase().split(' ').map(color => color.replace(',', ''));
+
+          // Check if the product's gender matches the selected filters
+          const genderFilterMatch =
+            (this.filteredProducts.includes('men') && productGender.includes('homme')) ||
+            (this.filteredProducts.includes('women') && productGender.includes('femme')) ||
+            (this.filteredProducts.includes('unisex') && productGender.includes('mixte'));
+
+          // Check if the product's price matches the selected filters
+          const priceFilterMatch =
+            (this.rangedProducts.includes('fifty') && productPrice < 50) ||
+            (this.rangedProducts.includes('fiftytohundred') && productPrice >= 50 && productPrice < 100) ||
+            (this.rangedProducts.includes('hundredtofifty') && productPrice >= 100 && productPrice < 150) ||
+            (this.rangedProducts.includes('hundredfifty') && productPrice >= 150);
+
+          // Check if the product's color matches the selected filters
+          const colorFilterMatch = 
+            (this.coloredProducts.length === 0) ||
+            (colorArray.some(color => this.coloredProducts.includes(color)));
+
+          // Check if the product's sport matches the selected filters
+          const sportFilterMatch = 
+            (this.sportProducts.length === 0) ||
+            (this.sportProducts.some(sport => productSport.includes(sport)));
+
+          if (this.sportProducts.length === 0) return false;
+
+          // Return true only if all filters match
+          return genderFilterMatch && priceFilterMatch && colorFilterMatch && sportFilterMatch;
+        }) : '';
+      },
+    },
+    methods: {
+      updateDataByFilter(updatedOptions) {
+        this.filteredProducts = updatedOptions;
+      },
+      updateDataByColor(updatedOptions) {
+        this.coloredProducts = updatedOptions;
+      },
+      updateDataByRange(updatedOptions) {
+        this.rangedProducts = updatedOptions;
+      },
+      updateDataBySport(updatedOptions) {
+        this.sportProducts = updatedOptions;
+      },
+      getNumberFromPrice(price) {
+        return parseFloat(price.slice(0, -2).replace(',', '.'));
+      },
+      getCurrentProductDisplayed() {
+        return this.displayProductsList.length;
+      },
+    }
+  }
+</script>
+
 <style scoped>
 header {
-  background-color: blue;
+  background-color: #f3f3f3;
   height: 80px;
   width: auto;
 }
@@ -31,13 +109,13 @@ main {
 }
 
 aside {
-  background-color: red;
+  background-color: #f3f3f3;
   width: 20%;
   height: auto;
 }
 
 section {
-  background-color: green;
+  background-color: #f3f3f3;
   width: 80%;
   height: auto;
 }
